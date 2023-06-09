476150c6780bd45da7a3bdb7d335636b39064978
Tania Andzani <2006134@itg.ac.id>
2023-06-09 12:56:28 +0700

    Menambahkan Post_Controller

---
+++ lib/config/constant.dart
@@ -0,0 +1,5 @@
+class ApiConstant {
+  static String baseUrl = "https://jsonplaceholder.typicode.com";
+  static String userEndpoint = "/users";
+  static String postEndpoint = "/posts";
+}
---
+++ lib/controller/post_controller.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:costume_api/Models/post.dart';
+import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+
+import '../config/constant.dart';
+
+class PostController {
+  Future<List<Post>> getPostData() async {
+    Uri url = Uri.parse(ApiConstant.baseUrl + ApiConstant.postEndpoint);
+    try {
+      Response response = await http.get(url);
+      // List<Map<String, dynamic>> posts = await getPosts();
+      List dataDecode = jsonDecode(response.body);
+      List<Post> dataposting = dataDecode.map((e) => Post.fromJson(e)).toList();
+      return dataposting;
+    } catch (error) {
+      throw Exception(error);
+    }
+  }
+}
